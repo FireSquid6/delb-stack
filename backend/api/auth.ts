94ead747cab9618a6aafd384d107ea6e1f7a4f4a
firesquid6 <jdeiss06@gmail.com>
2024-03-07 18:07:49 -0600

    add session attributes

--- backend/api/auth.ts
+++ backend/api/auth.ts
@@ -7,4 +7,19 @@ const adapter = new DrizzleSQLiteAdapter(db, sessions, users);
 
 export const auth = new Lucia(adapter, {
   sessionExpiresIn: new TimeSpan(4, "w"),
+  getSessionAttributes: (attributes) => {
+    return {
+      ipCountry: attributes.ip_country,
+    };
+  },
 });
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof auth;
+    DatabaseSessionAttributes: DatabaseSessionAttributes;
+  }
+  interface DatabaseSessionAttributes {
+    ip_country: string;
+  }
+}
