6976bf195f99922be7305e41008eb0705bb539ea
firesquid6 <jdeiss06@gmail.com>
2024-03-17 16:12:00 -0500

    try to add header of cookie

--- drizzle-turso-example/frontend/src/App.tsx
+++ drizzle-turso-example/frontend/src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -18,8 +15,19 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          onClick={async () => {
+            const result = await fetch("http://localhost:4000/session", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+              },
+              body: "email=jdeiss06@gmail.com&password=Jonathan12345",
+            });
+            console.log(result);
+          }}
+        >
+          Make a request!
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
@@ -29,7 +37,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- drizzle-turso-example/index.ts
+++ drizzle-turso-example/index.ts
@@ -2,11 +2,13 @@ import { Elysia } from "elysia";
 import "dotenv/config";
 import { fileRouter } from "delb-file-router";
 import cookie from "@elysiajs/cookie";
+import cors from "@elysiajs/cors";
 
 const app = new Elysia();
 
 app.use(fileRouter({ directory: "./api" }));
 app.use(cookie());
+app.use(cors());
 
 app.listen(4000, () => {
   console.log("🚀 Server is running on http://localhost:4000");
